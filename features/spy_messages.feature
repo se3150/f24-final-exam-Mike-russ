05cc53b95dae7da550bd4a05cfec45762de16151
Michael Russell <d00301670@utahtech.edu>
2024-12-09 14:36:19 -0700

    next fix for behave testing

--- features/spy_messages.feature
+++ features/spy_messages.feature
@@ -6,17 +6,11 @@ So that I can chat with my spy friends like a pro.
 Scenario: I can successfully encode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
     # write your steps here
-    when I select the option with the text "Encode" for element "decoder-setting"
-    and I select the option with the value "3" for element "shift-amount"
-    and I set "Secret Message To Decode" to the inputfield "letters"
-    and I submit the form "submit"
-    then I expect that "decoded_message" contains the text "Vhfuhw Phvvdjh Wr Ghfrgh"
+    when I enter a valid message
+    then I should get an encoded message
 
 Scenario: I can successfully decode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
     # write your steps here
-    when I select the option with the text "Decode" for element "decoder-setting"
-    and I select the option with the value "3" for element "shift-amount"
-    and I set "Vhfuhw Phvvdjh Wr Ghfrgh" to the inputfield "letters"
-    and I submit the form "submit"
-    then I expect that "decoded_message" contains the text "Secret Message To Decode"
+    when I enter an encoded message
+    then I should get a decoded message
--- features/steps/test.py
+++ features/steps/test.py
@@ -1,46 +1,46 @@
-# from behave import given, when, then
-# from selenium.webdriver.common.by import By
-# import time
+from behave import given, when, then
+from selenium.webdriver.common.by import By
+import time
 
-# @given('I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"')
-# def step_impl(context):
-#     context.behave_driver.get("https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder")
-#     time.sleep(2)  # Wait for page to load
+@given('I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"')
+def step_impl(context):
+    context.behave_driver.get("https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder")
+    time.sleep(2)  # Wait for page to load
 
-# @when("when I enter a valid message")
-# def step_impl(context):
-#     type = context.behave_driver.find_element(By.ID, "decoder-setting")
-#     shift = context.behave_driver.find_element(By.ID, "shift-amount")
-#     message = context.behave_driver.find_element(By.ID, "letters")
-#     submit_button = context.behave_driver.find_element(By.CLASS_NAME, "submit")
+@when("when I enter a valid message")
+def step_impl(context):
+    type = context.behave_driver.find_element(By.ID, "decoder-setting")
+    shift = context.behave_driver.find_element(By.ID, "shift-amount")
+    message = context.behave_driver.find_element(By.ID, "letters")
+    submit_button = context.behave_driver.find_element(By.CLASS_NAME, "submit")
 
-#     # Enter values
-#     type.send_keys("Encode")
-#     shift.send_keys("3")
-#     message.send_keys("Secret Message To Decode")
-#     submit_button.click()
-#     time.sleep(2)  # Wait for results to load
+    # Enter values
+    type.select_by_index(0)
+    shift.select_by_index(4)
+    message.send_keys("Secret Message To Decode")
+    submit_button.click()
+    time.sleep(2)  # Wait for results to load
 
-# @then("I should get an encoded message")
-# def step_impl(context):
-#     result = context.behave_driver.find_element(By.ID, "decoded_message").text
-#     assert result == "Vhfuhw Phvvdjh Wr Ghfrgh"
+@then("I should get an encoded message")
+def step_impl(context):
+    result = context.behave_driver.find_element(By.ID, "decoded_message").text
+    assert result == "Vhfuhw Phvvdjh Wr Ghfrgh"
 
-# @when("I enter an encoded message")
-# def step_impl(context):
-#     type = context.behave_driver.find_element(By.ID, "decoder-setting")
-#     shift = context.behave_driver.find_element(By.ID, "shift-amount")
-#     message = context.behave_driver.find_element(By.ID, "letters")
-#     submit_button = context.behave_driver.find_element(By.CLASS_NAME, "submit")
+@when("I enter an encoded message")
+def step_impl(context):
+    type = context.behave_driver.find_element(By.ID, "decoder-setting")
+    shift = context.behave_driver.find_element(By.ID, "shift-amount")
+    message = context.behave_driver.find_element(By.ID, "letters")
+    submit_button = context.behave_driver.find_element(By.CLASS_NAME, "submit")
 
-#     # Enter values
-#     type.send_keys("Decode")
-#     shift.send_keys("3")
-#     message.send_keys("Vhfuhw Phvvdjh Wr Ghfrgh")
-#     submit_button.click()
-#     time.sleep(2)  # Wait for results to load
+    # Enter values
+    type.select_by_index(1)
+    shift.select_by_index(4)
+    message.send_keys("Vhfuhw Phvvdjh Wr Ghfrgh")
+    submit_button.click()
+    time.sleep(2)  # Wait for results to load
 
-# @then("then I should get a decoded message")
-# def step_impl(context):
-#     result = context.behave_driver.find_element(By.ID, "decoded_message").text
-#     assert result == "Secret Message To Decode"
+@then("then I should get a decoded message")
+def step_impl(context):
+    result = context.behave_driver.find_element(By.ID, "decoded_message").text
+    assert result == "Secret Message To Decode"
