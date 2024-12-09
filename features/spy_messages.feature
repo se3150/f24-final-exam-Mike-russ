8480d006b1a917d3694a47b8429b40c2162322b0
Michael Russell <d00301670@utahtech.edu>
2024-12-09 13:51:22 -0700

    initial turn in for final

--- features/spy_messages.feature
+++ features/spy_messages.feature
@@ -6,7 +6,11 @@ So that I can chat with my spy friends like a pro.
 Scenario: I can successfully encode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
     # write your steps here
+    when I enter a valid message
+    then I should get an encoded message
 
 Scenario: I can successfully decode a secret message
     Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
     # write your steps here
+    when I enter an encoded message
+    then I should get a decoded message
--- test_brute.py
+++ test_brute.py
@@ -1,5 +1,7 @@
 import pytest
 from brute import Brute
+from unittest.mock import Mock
+from unittest.mock import patch
 
 todo = pytest.mark.skip(reason='todo: pending spec')
 
@@ -8,11 +10,38 @@ def describe_Brute():
     @pytest.fixture
     def cracker():
         return Brute("TDD")
+    
+    # @patch('brute')
 
     def describe_bruteOnce():
         # write your test cases here
-        pass
+        def it_returns_true_if_correct():
+            brute = Brute("testing string")
+            assert brute.bruteOnce("testing string") == True
+
+        def it_fails_if_first_brute_is_not_correct():
+            brute = Brute("testing string")
+            assert brute.bruteOnce("wrong") == False
+
+        def it_only_tests_once():
+            with patch.object(Brute, 'bruteOnce') as mock_brute:
+                thing = Brute("testing string")
+                thing.bruteOnce("testing string")
+            mock_brute.assert_called_once()
+            
 
     def describe_bruteMany():
         # write your test cases here
-        pass
+        def it_runs_without_crashing():
+            brute = Brute("hello")
+            assert brute.bruteMany(10) == -1
+
+        def it_returns_the_time_when_successful():
+            brute = Brute("a")
+            assert brute.bruteMany(10000000) != -1
+
+        def it_tests_many_times():
+            with patch.object(Brute, 'bruteMany') as mock_brute:
+                thing = Brute("testing string")
+                thing.bruteMany("testing string")
+            mock_brute.assert_called_once()
